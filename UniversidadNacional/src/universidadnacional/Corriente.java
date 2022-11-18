0ed3e9036bca7ab6d57ef52d0dd0f7686e2c854c
Kevin Guio <64978456+KevinGuio@users.noreply.github.com>
2022-11-17 19:22:10 -0500

    Delete UniversidadNacional directory

--- UniversidadNacional/src/universidadnacional/Corriente.java
+++
@@ -1,9 +0,0 @@
-package universidadnacional;
-
-public class Corriente {
-    public static double calcular_corriente(double voltaje, double resistencia){
-        double corriente;
-        corriente = voltaje/resistencia;
-        return corriente;
-    }
-}
--- UniversidadNacional/src/universidadnacional/Voltaje.java
+++
@@ -1,9 +0,0 @@
-package universidadnacional;
-
-public class Voltaje {
-    public static double calcular_Voltaje(double corriente, double resistencia){
-        double voltaje;
-        voltaje = corriente * resistencia;
-        return voltaje;
-    }
-}
